8d9a90dd5b76a98dee70b99dbce56b6f32d44eaf
myDiamondsDancing <66516985+dmitriyKolmogorov@users.noreply.github.com>
2020-07-27 16:17:26 +0300

    Add files via upload
    Create setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='vkvisualization',
+      version='0.0.1',
+      description='Visualize VK group data using Python.',
+      author='Dmitriy (myDiamondsDancing) Kolmogorov',
+      author_email='kolmogorov.dmitro@gmail.com',
+      packages=find_packages(),
+      url='https://github.com/dmitriyKolmogorov/VKVisualization')
